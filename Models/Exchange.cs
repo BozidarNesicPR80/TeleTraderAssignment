d58243cc569e5277b5da9b45864e30a6429b374c
BozidarNesicPR80 <neskebroo@gmail.com>
2023-11-11 18:06:10 +0100

    Added Views and Models

---
+++ Models/Exchange.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeleTraderAssignment.Models
+{
+    public class Exchange
+    {
+        public int Id { get; }
+        public string Name { get; }
+    }
+}
---
+++ Models/Symbol.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeleTraderAssignment.Models
+{
+    public class Symbol
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Ticker { get; set; }
+        public string Isin { get; set; }
+        public string CurrencyCode { get; set; }
+        public DateTime DateAdded { get; set; }
+        public float Price { get; set; }
+        public DateTime PriceDate { get; set; }
+
+        public Type Type { get; set; }
+        public int TypeId { get; set; }
+
+        public Exchange Exchange { get; set; }
+        public int ExchangeId { get; set; }
+    }
+}
