361cea7e3c3cf46abde8e7a2641d58a1c828ea7d
BozidarNesicPR80 <neskebroo@gmail.com>
2023-11-13 20:40:10 +0100

    Validation for forms

---
+++ Helpers/EmptyValidation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace TeleTraderAssignment.Helpers
+{
+    public class EmptyValidation : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (value is string str && !string.IsNullOrWhiteSpace(str))
+            {
+                return ValidationResult.ValidResult;
+            }
+
+            return new ValidationResult(false, "Field cannot be empty.");
+        }
+    }
+
+}
